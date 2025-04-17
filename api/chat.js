a0efb249c590b028895a3dd40fddb9a983fa5011
Pentala, Venu Madhav <vpentala@iu.edu>
2025-04-17 01:20:22 -0400

    ds

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
 module.exports = async (req, res) => {
   // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "https://madhavvan.github.io/PortfolioWebsite/"); 
+  res.setHeader("Access-Control-Allow-Origin", "*"); 
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
